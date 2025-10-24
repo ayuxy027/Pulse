05fbc63fefde6703b9dd7a92e0b879054fffe0c1
Sumeet_Gond <sumitv9009@gmail.com>
2025-10-25 03:45:53 +0530

    fix: remove deleted page exports from index
    - Remove CasesPage export (file deleted)
    - Remove InboxPage export (file deleted)
    - Add HomePage export
    - Clean up after merge from master

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,6 @@
 // Page Components
 export { default as DashboardPage } from './DashboardPage';
 export { default as ChatPage } from './ChatPage';
-export { default as CasesPage } from './CasesPage';
-export { default as InboxPage } from './InboxPage';
 export { default as DietPage } from './DietPage';
 export { default as ScannerPage } from './ScannerPage';
+export { default as HomePage } from './HomePage';
